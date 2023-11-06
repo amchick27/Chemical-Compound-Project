e33b414c17a67e4bd212c2dd556d9222c8aaf30b
Abby Chicken <146774912+amchick27@users.noreply.github.com>
2023-11-06 16:52:15 -0600

    Update mainFile.cpp
    added starting code

--- mainFile.cpp
+++ mainFile.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+// function that converts one intput to a related but different output. 
+string conversion (string inputArray[], string outputArray[]);
+
+int main(){
+  string userInputArray[] = {0};
+  string returnArray[] = {0};
+  cout << "enter three words" << endl;
+  cin.getline(userInputArray);
+
+  return 0;
+}
+
+string conversion (inputArray[], outputArray[]){
+  
+}
